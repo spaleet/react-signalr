34edbc8ee60f31e06164fa427bd9cb2a42d03b99
Ali M <ali.mo.dev@gmail.com>
2022-09-13 19:23:19 +0430

    Lobby redesign

--- client/src/components/Lobby.tsx
+++ client/src/components/Lobby.tsx
@@ -1,6 +1,6 @@
-import { Button, FormGroup, TextField } from '@mui/material'
-import { Box } from '@mui/system'
-import { useState } from 'react'
+import { Grid, Divider, Box, Button, TextField, Paper, Typography, Avatar } from '@mui/material';
+import { useState } from 'react';
+import SendIcon from '@mui/icons-material/Send';
 
 interface LobbyProps {
     joinRoom: (username: string, room: string) => void,
@@ -12,25 +12,53 @@ const Lobby = (props: LobbyProps) => {
     const [room, setRoom] = useState("");
 
     return (
-        <Box 
-            component="form"
-            onSubmit={e => {
-                e.preventDefault();
-                props.joinRoom(username, room)
-            }}
-        >
+        <Grid sx={{
+            minWidth: "100%", minHeight: "100vh", padding: "20px",
+            display: "flex", flexDirection: "column", justifyContent: "center", alignItems: 'center',
+        }}>
 
-            <FormGroup>
-                <TextField placeholder="username" variant="outlined"
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <TextField placeholder="room" variant="outlined"
-                    onChange={e => setRoom(e.target.value)} />
-            </FormGroup>
-            
-            <Button variant="contained" type="submit" disabled={!username || !room}>Join</Button>
+            <Paper sx={{ 
+                p: 3, boxShadow: 2, display: 'flex', flexDirection: 'column',
+                width: { xs: '100%', sm: '100%', md: '430px' }, alignItems: 'center'
+            }} >
 
-        </Box>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <SendIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Join a room
+                </Typography>
+
+                <Divider variant='fullWidth' sx={{ mt: 1, mb: 2 }} />
+
+                <Box
+                    component="form"
+                    onSubmit={e => {
+                        e.preventDefault();
+                        props.joinRoom(username, room)
+                    }}
+                >
+                    <Box sx={{ pr: 3, pl: 3 }}>
+                        <TextField
+                            label="Username" fullWidth
+                            type="text" required margin="dense"
+                            onChange={e => setUsername(e.target.value)} />
+
+                        <TextField
+                            label="Room" fullWidth
+                            type="text" required margin="dense"
+                            onChange={e => setRoom(e.target.value)} />
+                    </Box>
+
+                    <Button variant="contained" type="submit" fullWidth sx={{ mt: 3, mb: 2 }}>
+                        Join
+                    </Button>
+                </Box>
+
+            </Paper>
+
+        </Grid>
+
     )
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from '@mui/material/CssBaseline';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,6 +8,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
+        <CssBaseline />
         <App />
     </React.StrictMode>
 );
