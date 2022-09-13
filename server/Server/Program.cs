c1461b52f11ae7a5a545d9fef6ed377d1f1de1af
Ali M <ali.mo.dev@gmail.com>
2022-09-13 19:27:32 +0430

    added CORS

--- server/Server/Program.cs
+++ server/Server/Program.cs
@@ -3,7 +3,20 @@ using Server.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(builder => 
+    {
+        builder.WithOrigins("http://localhost:3000")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapGet("/", () => "Hello World!");
 app.MapHub<ChatHub>("/chat");
