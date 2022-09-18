09b54d6a5e3ae0b138615e5450704d5f68b729b9
Ali M <ali.mo.dev@gmail.com>
2022-09-18 16:15:21 +0430

    added alert and backdrop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,17 @@
 import { Lobby, Chats } from './pages/_index';
-import { Box, Container, Grid } from '@mui/material';
+import { Box, Container, Grid, Snackbar, Alert } from '@mui/material';
 import { useState, useEffect } from 'react';
 import { HubConnection } from '@microsoft/signalr/dist/esm/HubConnection';
 import { buildConnection, startConnection } from './utils/hubUtils';
 import { Message } from './models/Message';
 
-const defaultData: Message[] = [
-    { username: "sfds", message: "dfd" },
-    { username: "gff", message: "dfd" },
-    { username: "sdf", message: "dfd" },
-]
-
 const App = () => {
 
     const [connection, setConnection] = useState<HubConnection>();
     const [inRoom, setInRoom] = useState(false);
-    const [messages, setMessages] = useState<Message[]>(defaultData);
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const [errorAlertOpen, setErrorAlertOpen] = useState(false);
 
     useEffect(() => {
         const newConnection = buildConnection();
@@ -31,7 +27,7 @@ const App = () => {
                 connection.on("ReceiveMessage", (username, message) => {
                     setMessages(messages => [...messages, { username, message }]);
                 });
-            
+
             }
         }
 
@@ -39,23 +35,37 @@ const App = () => {
     }, [connection]);
 
     return (
-        <Box sx={{ width: "100%", height: '100vh' }}>
-            <Container>
-                <Box sx={{
-                    minWidth: "100%", minHeight: "100vh", padding: "20px",
-                    display: "flex", flexDirection: "column", justifyContent: "center", alignItems: 'center',
-                }}>
-                    {connection &&
-                        <>
-                            {!inRoom
-                                ? <Lobby connection={connection} onJoined={setInRoom} />
-                                : <Chats connection={connection} messages={messages} />
-                            }
-                        </>
-                    }
-                </Box>
-            </Container>
-        </Box>
+        <>
+            <Box sx={{ width: "100%", height: '100vh' }}>
+                <Container>
+                    <Box sx={{
+                        minWidth: "100%", minHeight: "100vh", padding: "20px",
+                        display: "flex", flexDirection: "column", justifyContent: "center", alignItems: 'center',
+                    }}>
+                        {connection &&
+                            <>
+                                {!inRoom
+                                    ? <Lobby connection={connection}
+                                        onJoined={(success) => {
+
+                                            if (!success) {
+                                                setErrorAlertOpen(true)
+                                            }
+                                            setInRoom(success)
+                                        }} />
+                                    : <Chats connection={connection} messages={messages} />}
+                            </>}
+                    </Box>
+                </Container>
+            </Box>
+
+            <Snackbar open={errorAlertOpen} autoHideDuration={1500}>
+                <Alert severity="error" sx={{ width: '100%' }} variant="filled">
+                    Couldn't join the room!
+                </Alert>
+            </Snackbar>
+
+        </>
     )
 };
 
--- client/src/pages/Lobby/Lobby.tsx
+++ client/src/pages/Lobby/Lobby.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, TextField, Paper, Typography, Avatar } from '@mui/material';
+import { Box, Button, TextField, Paper, Typography, Avatar, Backdrop, CircularProgress } from '@mui/material';
 import React, { useState } from 'react';
 import SendIcon from '@mui/icons-material/Send';
 import { HubConnection } from '@microsoft/signalr/dist/esm/HubConnection';
@@ -14,56 +14,68 @@ const Lobby = (props: LobbyProps) => {
     const [username, setUsername] = useState("");
     const [room, setRoom] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const [open, setOpen] = useState(false);
 
-        try {
-            await props.connection.invoke("JoinRoom", { username, roomId: room });
-            props.onJoined(true);
-        } catch (e) {
-            console.log("Join room failed. : ", e);
-            props.onJoined(false);
-        }
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+        e.preventDefault();
+        setOpen(true);
+
+        props.connection.invoke("JoinRoom", { username, roomId: room })
+            .then(() => props.onJoined(true))
+            .catch((e) => {
+                console.log("Join room failed. : ", e);
+                props.onJoined(false);
+            })
+            .finally(() => setOpen(false));
     }
 
     return (
-        <Paper sx={{
-            p: 3, boxShadow: 2, display: 'flex', flexDirection: 'column',
-            width: { xs: '100%', sm: '60%', md: '430px' }, alignItems: 'center',
-            borderRadius: '1.7rem'
-        }} >
+        <>
+            <Paper sx={{
+                p: 3, boxShadow: 2, display: 'flex', flexDirection: 'column',
+                width: { xs: '100%', sm: '60%', md: '430px' }, alignItems: 'center',
+                borderRadius: '1.7rem'
+            }}>
 
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                <SendIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5" color="primary.main">
-                Join a room
-            </Typography>
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <SendIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5" color="primary.main">
+                    Join a room
+                </Typography>
 
-            <ChatDivider width="62%" />
+                <ChatDivider width="62%" />
 
-            <Box
-                component="form"
-                onSubmit={handleSubmit}
-            >
-                <Box sx={{ pr: 3, pl: 3 }}>
-                    <TextField
-                        label="Username" fullWidth
-                        type="text" required margin="dense"
-                        onChange={e => setUsername(e.target.value)} />
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                >
+                    <Box sx={{ pr: 3, pl: 3 }}>
+                        <TextField
+                            label="Username" fullWidth
+                            type="text" required margin="dense"
+                            onChange={e => setUsername(e.target.value)} />
 
-                    <TextField
-                        label="Room" fullWidth
-                        type="text" required margin="dense"
-                        onChange={e => setRoom(e.target.value)} />
+                        <TextField
+                            label="Room" fullWidth
+                            type="text" required margin="dense"
+                            onChange={e => setRoom(e.target.value)} />
+                    </Box>
+
+                    <Button variant="contained" type="submit" fullWidth sx={{ mt: 3, mb: 2 }}>
+                        Join
+                    </Button>
                 </Box>
+            </Paper>
 
-                <Button variant="contained" type="submit" fullWidth sx={{ mt: 3, mb: 2 }}>
-                    Join
-                </Button>
-            </Box>
-
-        </Paper>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={open}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </>
     )
 }
 
