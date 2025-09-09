b1c968a5dbc3af9a13154dbb074b8d0acae40f50
spaleet <ali.mo.dev@gmail.com>
2025-09-09 11:14:22 +0330

    react 19 upgrade

---
+++ src/client/src/main.tsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import lightTheme from './themes/lightTheme';
+import { HubContextProvider } from './contexts/HubContext';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider theme={lightTheme} >
+      <CssBaseline />
+
+      <HubContextProvider>
+        <App />
+      </HubContextProvider>
+
+    </ThemeProvider>
+  </StrictMode>
+)
